0c94da69606e78e201d88e746596f246717e753c
carrot <viktoriya.korekina@simpleone.ru>
2026-08-13 09:51:59 +0300

    chore: switch to ten version and migrate database

---
+++ FormApp.Api/Migrations/20260812203154_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SenderCity = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    SenderAddress = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    RecipientCity = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    RecipientAddress = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Weight = table.Column<int>(type: "INTEGER", maxLength: 50, nullable: false),
+                    Date = table.Column<int>(type: "INTEGER", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
--- FormApp.Api/Program.cs
+++ FormApp.Api/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 string connectionString = builder.Configuration.GetConnectionString
 ("Default") ?? throw new ArgumentNullException("connectionString is null");
@@ -24,12 +23,6 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.UseCors("ReactDev");
